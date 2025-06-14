61924739043b873e628e1a4859b5ffc3870f551c
danielbelle <henrique.danielb@gmail.com>
2025-06-14 01:53:28 -0300

    0.4.2 - fix headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,16 @@ const nextConfig = {
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          { key: "Content-Security-Policy", value: "default-src 'self'" },
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self';",
+              "script-src 'self' https://www.google.com https://www.gstatic.com;",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+              "font-src 'self' https://fonts.gstatic.com;",
+              "frame-src https://www.google.com https://www.gstatic.com;",
+            ].join(" "),
+          },
         ],
       },
     ];
